7d8a8af03335cab96ef2ddcca6667ee933342274
Samarth Agarwal <samarthagarwal@live.com>
2024-09-01 23:16:32 +0530

    fix: add obj parser, added porsche model, gesture

--- lib/main.dart
+++ lib/main.dart
@@ -42,15 +42,59 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
+  double angleX = 0;
+  double angleY = 0;
+  double _previousX = 0;
+  double _previousY = 0;
+
+  void _updateCube(DragUpdateDetails data) {
+    if (angleY > 360.0) {
+      angleY = angleY - 360.0;
+    }
+    if (_previousY > data.globalPosition.dx) {
+      setState(() {
+        angleY = angleY + 0.025;
+      });
+    }
+    if (_previousY < data.globalPosition.dx) {
+      setState(() {
+        angleY = angleY - 0.025;
+      });
+    }
+    _previousY = data.globalPosition.dx;
+
+    if (angleX > 360.0) {
+      angleX = angleX - 360.0;
+    }
+    if (_previousX > data.globalPosition.dy) {
+      setState(() {
+        angleX = angleX - 0.025;
+      });
+    }
+    if (_previousX < data.globalPosition.dy) {
+      setState(() {
+        angleX = angleX + 0.025;
+      });
+    }
+    _previousX = data.globalPosition.dy;
+  }
+
+  void _updateY(DragUpdateDetails data) {
+    _updateCube(data);
+  }
+
+  void _updateX(DragUpdateDetails data) {
+    _updateCube(data);
+  }
 
   @override
   void initState() {
     super.initState();
     _animationController = AnimationController(vsync: this, duration: const Duration(seconds: 20));
-    _animationController.addListener(() {
-      setState(() {});
-    });
-    _animationController.repeat();
+    // _animationController.addListener(() {
+    //   setState(() {});
+    // });
+    // _animationController.repeat();
   }
 
   @override
@@ -62,24 +106,31 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.white,
       body: FutureBuilder<Model>(future: () async {
         ObjParser objParser = ObjParser();
-        await objParser.loadObj("assets/models/Jeep_Renegade_2016.obj");
+        await objParser.loadObj("assets/models/Porsche_911_GT2.obj");
         Model modelToDraw = Model(points: objParser.vertices, lines: objParser.faces);
         return modelToDraw;
       }(), builder: (context, snapshot) {
         return Center(
-          child: Container(
-            color: Colors.white.withOpacity(0.1),
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: Center(
-              child: CustomPaint(
-                painter: CubePainter(
-                  angle: _animationController.value * 2 * pi,
-                  side: min(MediaQuery.of(context).size.width / 2, MediaQuery.of(context).size.height / 2),
-                  model: snapshot.data ?? sphere,
+          child: GestureDetector(
+            onHorizontalDragUpdate: _updateY,
+            onVerticalDragUpdate: _updateX,
+            child: Container(
+              color: Colors.white.withOpacity(0.1),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Center(
+                child: RepaintBoundary(
+                  child: CustomPaint(
+                    painter: CubePainter(
+                      angleX: 170,
+                      angleY: angleY,
+                      side: min(MediaQuery.of(context).size.width / 2, MediaQuery.of(context).size.height / 2),
+                      model: snapshot.data ?? sphere,
+                    ),
+                  ),
                 ),
               ),
             ),
@@ -91,11 +142,12 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
 }
 
 class CubePainter extends CustomPainter {
-  final double angle;
+  final double angleX;
+  final double angleY;
   final double side;
   final Model model;
 
-  CubePainter({this.angle = 0.0, this.side = 100, required this.model});
+  CubePainter({this.angleX = 0.0, this.angleY = 0.0, this.side = 100, required this.model});
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -116,16 +168,16 @@ class CubePainter extends CustomPainter {
     }).toList();
 
     // Rotate the points around the Y-axis
-    final rotationMatrix = vector_math.Matrix4.rotationY(angle);
+    final rotationMatrix = vector_math.Matrix4.rotationY(angleY);
     // convert degrees to radians
-    const degrees = 150;
-    const radians = degrees * pi / 180;
-    final rotationMatricX = vector_math.Matrix4.rotationX(radians);
+    double degrees = angleX;
+    double radians = degrees * pi / 180;
+    final rotationMatrixX = vector_math.Matrix4.rotationX(radians);
 
     final transformedPoints = points.map((point) {
       // final transformedPoint = point.clone();
       var transformedPoint = rotationMatrix.transform3(point);
-      transformedPoint = rotationMatricX.transform3(transformedPoint);
+      transformedPoint = rotationMatrixX.transform3(transformedPoint);
       // Apply perspective projection
       // double perspective = (2 + (transformedPoint.z / (side * 2)));
       double perspective = 1 / (2 - transformedPoint.z / (side * 2));
@@ -146,7 +198,7 @@ class CubePainter extends CustomPainter {
     // draw all transformed points
     for (int i = 0; i < transformedPoints.length; i++) {
       // calculate the radius of the point based on the z value
-      double radius = 0.5 * transformedPoints[i].z / side / 2; //1.25 + 3 * transformedPoints[i].z / side / 2;
+      double radius = 0.2 * transformedPoints[i].z / side / 2; //1.25 + 3 * transformedPoints[i].z / side / 2;
       try {
         canvas.drawCircle(Offset(transformedPoints[i].x ?? 0, transformedPoints[i].y ?? 0), radius, paint..style = PaintingStyle.fill);
       } catch (e) {
@@ -158,20 +210,60 @@ class CubePainter extends CustomPainter {
     // Draw the edges of the cube
     for (var i = 0; i < model.lines.length; ++i) {
       final line = model.lines[i]; // [0, 1]
-      final startPoint = transformedPoints[line[0]];
-      final endPoint = transformedPoints[line[1]];
-      _drawLine(canvas, paint, startPoint, endPoint);
+      final v1 = transformedPoints[line[0]];
+      final v2 = transformedPoints[line[1]];
+      Vector3 v3;
+      if (line.length == 3) {
+        v3 = transformedPoints[line[2]];
+      } else {
+        v3 = transformedPoints[line[0]];
+      }
+
+      // culling back faces
+      final normal = calculateNormal(v1, v2, v3);
+
+      final viewDirection = Vector3(0, 0, -1);
+
+      if (!isFaceVisible(normal, viewDirection)) {
+        continue; // Skip rendering this face
+      }
+
+      for (var j = 0; j < line.length; j += 1) {
+        final start = transformedPoints[line[j]];
+        final end = transformedPoints[line[(j + 1) % line.length]];
+        _drawLine(i, canvas, paint, start, end);
+      }
     }
   }
 
-  void _drawLine(Canvas canvas, Paint paint, vector_math.Vector3 p1, vector_math.Vector3 p2) {
+  void _drawLine(int index, Canvas canvas, Paint paint, vector_math.Vector3 p1, vector_math.Vector3 p2) {
     Offset o1 = Offset(p1.x, p1.y);
     Offset o2 = Offset(p2.x, p2.y);
-    canvas.drawLine(o1, o2, paint..color = Colors.white.withOpacity(clampDouble(p1.z * 5 / side, 0, 1)));
+
+    canvas.drawLine(
+      o1,
+      o2,
+      paint
+        ..color = Colors.black.withOpacity(clampDouble(p1.z / side, 0.5, 1))
+        ..strokeWidth = clampDouble(p1.z / side, 0.1, 0.2),
+    );
+  }
+
+  Vector3 calculateNormal(Vector3 v1, Vector3 v2, Vector3 v3) {
+    final edge1 = v2 - v1;
+    final edge2 = v3 - v1;
+    final normal = edge1.cross(edge2);
+    normal.normalize();
+    return normal;
+  }
+
+  bool isFaceVisible(Vector3 normal, Vector3 viewDirection) {
+    final dotProduct = normal.dot(viewDirection);
+    return dotProduct < 0;
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return angle != (oldDelegate as CubePainter).angle;
+    return angleX != (oldDelegate as CubePainter).angleX || angleY != (oldDelegate).angleY;
   }
 }
